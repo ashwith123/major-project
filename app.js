e8d785125f257e35b21d3e12b9f2d86e423c99c8
ashwith <ashwith9492@gmail.com>
2024-10-06 15:08:29 +0530

    created a sample listing which adds a list to collection or moudule

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const listing = require("./models/listing.js");
 const mongoose = require("mongoose");
 
 // checks if connection is successfull main is name of function in which connection is being given
@@ -23,4 +23,18 @@ app.listen(8080, (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("working");
+});
+
+// this is adding an new list to listing module created in listing.js and reuired here
+app.get("/listing", (req, res) => {
+  let samplelist = new listing({
+    title: "ashre",
+    descrition: "i am good",
+    price: 1200,
+    location: "goa",
+    country: "india",
+  });
+  samplelist.save();
+  console.log("data saved and working ");
+  res.send("sucessfull");
 });
--- models/listing.js
+++ models/listing.js
@@ -4,9 +4,20 @@ const schema = mongoose.Schema; // schema required from mongoose
 
 //schema created
 const listingschema = new schema({
-  title: String,
+  title: {
+    type: String,
+    required: true,
+  },
   description: String,
-  image: String,
+  image: {
+    type: String,
+    default:
+      "https://imgs.search.brave.com/yFLPbMIn1BcTxuf2Sqm0lzSzvXIsfTju2QOAnbQ-buk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzQzLzkwLzM3/LzM2MF9GXzQzOTAz/NzMyX1gxWjJnenk0/OTdhaXNnTUU2MmNy/bVUwMFNmOHNtUDU5/LmpwZw",
+    set: (v) =>
+      v === ""
+        ? "https://imgs.search.brave.com/yFLPbMIn1BcTxuf2Sqm0lzSzvXIsfTju2QOAnbQ-buk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzQzLzkwLzM3/LzM2MF9GXzQzOTAz/NzMyX1gxWjJnenk0/OTdhaXNnTUU2MmNy/bVUwMFNmOHNtUDU5/LmpwZw"
+        : v, // its like if else if v is empty then deualt else v
+  },
   price: Number,
   location: String,
   country: String,
@@ -14,4 +25,4 @@ const listingschema = new schema({
 
 // model for schema created
 const listing = mongoose.model("listing", listingschema);
-MediaSourceHandle.export = listing; // schema exported
+module.exports = listing; // module exported
