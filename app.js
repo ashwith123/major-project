345ae1a7d8ca19562891568ff8567419cb6ec049
ashwith <ashwith9492@gmail.com>
2024-10-06 16:53:09 +0530

    main ejs ecreated

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const listing = require("./models/listing.js");
 const mongoose = require("mongoose");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // checks if connection is successfull main is name of function in which connection is being given
 main()
@@ -25,16 +29,22 @@ app.get("/", (req, res) => {
   res.send("working");
 });
 
-// this is adding an new list to listing module created in listing.js and reuired here
-app.get("/listing", (req, res) => {
-  let samplelist = new listing({
-    title: "ashre",
-    descrition: "i am good",
-    price: 1200,
-    location: "goa",
-    country: "india",
-  });
-  samplelist.save();
-  console.log("data saved and working ");
-  res.send("sucessfull");
+app.get("/listing", async (req, res) => {
+  let alllistings = await listing.find({});
+  console.log(alllistings);
+  res.render("./listings/index.ejs", { alllistings });
 });
+
+// // this is adding an new list to listing module created in listing.js and reuired here
+// app.get("/listing", (req, res) => {
+//   let samplelist = new listing({
+//     title: "ashre",
+//     descrition: "i am good",
+//     price: 1200,
+//     location: "goa",
+//     country: "india",
+//   });
+//   samplelist.save();
+//   console.log("data saved and working ");
+//   res.send("sucessfull");
+// });
